94123c08d68374b17aad108dea72f8872ac4988a
GODA47 <kampanartsudprasert@gmail.com>
2021-08-15 21:13:19 +0700

    Compared Latesum

--- utils/SubwordEmbedding.py
+++ utils/SubwordEmbedding.py
@@ -6,29 +6,28 @@ class subwordembedding(Module):
                  num_embeddings, 
                  embedding_dim, 
                  device = torch.device("cpu"),
-                 padding_idx = 0):
+                 padding_idx = 0,
+                 sumfirst = True):
         super().__init__()
         self.num_embeddings = num_embeddings
         self.embedding_dim = embedding_dim
         self.device =  device
         self.padding_idx = padding_idx
-        
+        self.sumfirst = sumfirst
         self.subword_embedding = Embedding(num_embeddings = num_embeddings, 
                                            embedding_dim = embedding_dim, 
                                            padding_idx = padding_idx)
     def forward(self, token_ids):
-        # token_ids: (batch_size, word_num, hash_size)
-        # return: (batch_size, word_num, embedding_dim)
-        debug = False
+        # token_ids: (batch_size, hash_size)
         
         subword_embed = self.subword_embedding(token_ids)
-        # (batch_size, word_num, hash_size, embedding_dim)
-        if debug: print("subword_embed.shape: ", subword_embed.shape)
+        # (batch_size, ngram_size, embedding_dim)
         
-        word_embed = subword_embed.sum(dim = len(subword_embed.shape) -2).to(self.device)
-        # (batch_size, word_num, embedding_dim)
-        if debug: print("word_embed.shape: ", word_embed.shape)
-        
-        if debug: print("\n########################################\n")
+        if self.sumfirst:
+            word_embed = subword_embed.sum(dim = len(subword_embed.shape) -2).to(self.device)
+            # (batch_size, embedding_dim)
+        else:
+            word_embed = subword_embed.to(self.device)
+            # (batch_size, ngram_size, embedding_dim)
         return word_embed
         
